d2394325e0d9c999edd598ece5eb424b18e2c4cc
countwallace <kelvin.wallace@stu.cvccworks.edu>
2025-12-16 12:31:38 -0500

    Add files via upload

---
+++ projects/html/fitnessch12/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Kelvin Wallace
+    File Name: script.js
+    Date: December 12, 2022
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+	$('.hero').height($(window).height());
+}
