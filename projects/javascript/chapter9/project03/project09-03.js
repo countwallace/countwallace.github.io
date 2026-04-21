5ad0ab850997f99e5d70607559bac3a5281e1913
countwallace <kelvin.wallace@stu.cvccworks.edu>
2026-04-20 23:21:32 -0400

    Add files via upload

---
+++ projects/javascript/chapter9/project03/project09-03.js
@@ -0,0 +1,37 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-03
+
+      Project to retrieve date of last visit from web storage and mark new article
+      Author: Kelvin Wallace
+      Date: 4/20/2026
+
+      Filename: project09-03.js
+*/
+
+/* Page Objects */
+
+let lastVisitDate = document.getElementById("lastVisitDate");
+let articleDates = document.getElementsByClassName("posttime");
+
+if(localStorage.getItem("sbloggerVisit") !== null){
+    let storedLastDate = localStorage.getItem("sbloggerVisit");
+    lastVisitDate.textContent = storedLastDate;
+    let lastDate = new Date(storedLastDate);
+
+    for (let item of articleDates){
+        let articleDate = new Date(item.textContent);
+        if(articleDate > lastDate){
+            item.innerHTML += "<strong>new</strong>";
+        }
+    }
+}else{
+    lastVisitDate.textContent = "Welcome to SBlogger!";
+    for(let item of articleDates){
+        item.innerHTML += "<strong>new</strong>";
+    }
+}
+
+let currentDate = new Date("9/12/2024");
+localStorage.setItem("sbloggerVisit", currentDate.toLocaleDateString());
---
+++ projects/javascript/chapter9/project04/project09-04.js
@@ -0,0 +1,46 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-04
+
+      Project to store high scores from a game in a cookie
+      Author: Kelvin Wallace
+      Date: 4/20/2026
+
+      Filename: project09-04.js
+*/
+
+/* Page Objects */
+let bestText = document.getElementById("best");
+let clockTimer = document.getElementById("timer");
+
+// Custom event that runs when the puzzle is solved
+window.addEventListener("puzzleSolved", updateRecord);
+
+// Event listener that is run when the page loads
+window.addEventListener("load", function() {
+    if(document.cookie){
+        bestText.textContent = getBestTime() + " seconds";
+    }
+});
+
+function getBestTime(){
+    if(document.cookie){
+        let cookieArray = document.cookie.split("=");
+        return parseInt(cookieArray[1]);
+    }else{
+        return 9999;
+    }
+}
+
+function updateRecord(){
+    let solutionTime = parseInt(document.getElementById("timer").textContent);
+    let bestTime = getBestTime();
+
+    if(solutionTime < bestTime){
+        bestTime = solutionTime;
+    }
+    bestText.textContent = bestTime + " seconds";
+    document.cookie = "puzzle8Best=" + bestTime + "; max-age=7776000";
+}
+
